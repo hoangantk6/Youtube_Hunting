5c785f482ba6c79165f5636cc905438d50d0bef7
Phuc <phuc.le@ug.fuv.edu.vn>
2021-12-21 15:13:37 +0700

    separate general utils into another file

--- Utils/data_collection_utils.py
+++ Utils/data_collection_utils.py
@@ -1,30 +1,5 @@
-from datetime import datetime
 import pandas as pd
-import pytz
-
-def save_database(df, file_path):
-  """
-  save dataframe into a .csv file
-  """
-  df.to_csv(file_path, index=False)
-  print('Database is saved to', file_path)
-
-def update_database(original_df, new_df):
-  """
-  append new dataframe to old dataframe and avoid duplications in primary key
-  """
-  return original_df.append(new_df, ignore_index=True)
-
-def display_table(df, data_table):
-  """
-  display table in Google Colab
-  """
-  return data_table.DataTable(df, num_rows_per_page=10)
-
-def what_date_today():
-  today = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-  today = today.strftime("%Y-%m-%d %H:%M:%S")
-  return today
+import utils
 
 # ---- pyyoutube ----
 # CHANNEL
@@ -56,7 +31,7 @@ def channel_dict_to_df(channel_dict):
   channel['thumbnail_default'] = channel_dict['snippet']['thumbnails']['default']['url']
   channel['thumbnail_high'] = channel_dict['snippet']['thumbnails']['high']['url']
   channel['thumbnail_medium'] = channel_dict['snippet']['thumbnails']['medium']['url']
-  channel['last_update'] = what_date_today()
+  channel['last_update'] = utils.what_date_today()
   return pd.DataFrame([channel])
 
 # VIDEO
@@ -98,7 +73,7 @@ def video_dict_to_df(video_dict):
   video['thumbnail_medium'] = video_dict['snippet']['thumbnails']['medium']['url']
   video['thumbnail_high'] = video_dict['snippet']['thumbnails']['high']['url']
   video['thumbnail_standard'] = video_dict['snippet']['thumbnails']['standard']['url']
-  video['last_update'] = what_date_today()
+  video['last_update'] = utils.what_date_today()
 
   return pd.DataFrame([video])
 
---
+++ Utils/utils.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import pandas as pd
+import pytz
+
+def save_database(df, file_path):
+  """
+  save dataframe into a .csv file
+  """
+  df.to_csv(file_path, index=False)
+  print('Database is saved to', file_path)
+
+def update_database(original_df, new_df):
+  """
+  append new dataframe to old dataframe and avoid duplications in primary key
+  """
+  return original_df.append(new_df, ignore_index=True)
+
+def display_table(df, data_table):
+  """
+  display table in Google Colab
+  """
+  return data_table.DataTable(df, num_rows_per_page=10)
+
+def what_date_today():
+  today = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+  today = today.strftime("%Y-%m-%d %H:%M:%S")
+  return today
